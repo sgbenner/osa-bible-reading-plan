4654ebf282baa9972fefa062efa3111ae9721522
benner.steven@gmail.com <benner.steven@gmail.com>
2023-01-02 13:52:09 -0600

    improve scripture link feature

--- src/components/Content.js
+++ src/components/Content.js
@@ -2,28 +2,39 @@ import React from "react";
 import moment from "moment";
 import readingPlan from "../_data/readingPlan";
 
-const Content = ({ styles }) => {
-  
-  const contentStyle = {
-    paddingTop: styles.topBarHeight + 20,
-    paddingRight: 20,
-    paddingBottom: styles.footerMenuHeight + 20,
-    paddingLeft: 20
-  };
+const scriptureUrlPrefix = 'https://www.biblegateway.com/passage/?search='
+const scriptureUrlSuffix = '&version=ESV'
 
-  return (
-    <div style={contentStyle}>
-      {readingPlan.map((post, i) => {
-        return (
-          <div key={i} style={{ marginBottom: 40 }}>
-            <h2 style={{ marginBottom: 0 }}>{moment(post.day).format('dddd MMMM D, YYYY')}</h2>
-            <p>{post.oldTestament !== '' ? `Old Testament: ${post.oldTestament}`: '' }</p>
-            <p>{post.newTestament !== '' ? `New Testament: ${post.newTestament}` : '' }</p>
-          </div>
-        );
-      })}
-    </div>
-  );
+function createScriptpureLink (scripture) {
+    return scriptureUrlPrefix + encodeURIComponent(scripture) + scriptureUrlSuffix
+}
+
+const Content = ({ styles }) => {
+
+    const contentStyle = {
+        paddingTop: styles.topBarHeight + 20,
+        paddingRight: 20,
+        paddingBottom: styles.footerMenuHeight + 20,
+        paddingLeft: 20
+    };
+
+    return (
+        <div style={contentStyle}>
+            {readingPlan.map((post, i) => {
+                return (
+                    <div key={i} style={{ marginBottom: 40 }}>
+                        <h2 style={{ marginBottom: 0 }}>{moment(post.day).format('dddd MMMM D, YYYY')}</h2>
+                        {/* If OT script exists, add to section */}
+                        {post.oldTestament !== '' ? <p>{`Old Testament: ` }
+                            <a href={`${createScriptpureLink(post.oldTestament)}`} target="_blank" rel="noreferrer">{`${post.oldTestament}`}</a></p> : undefined}
+                        {/* If NT script exists, add to section */}
+                        {post.newTestament !== '' ? <p>{`New Testament: ` }
+                        <a href={`${createScriptpureLink(post.newTestament)}`} target="_blank" rel="noreferrer">{`${post.newTestament}`}</a></p> : undefined}
+                    </div>
+                );
+            })}
+        </div>
+    );
 };
 
 export default Content;
